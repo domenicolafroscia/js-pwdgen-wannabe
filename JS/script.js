16940d1d216f63937b9a5218fbef1c0a3506298d
domenicolafroscia <domenicolafroscia@gmail.com>
2023-10-17 15:15:52 +0200

    add: aggiunta raccolta dati in js

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+// Raccolta di dati
+const name = (prompt("Scrivi il tuo nome"));
+console.log(name, typeof name);
+const surname = (prompt("Scrivi il tuo cognome"));
+console.log(surname, typeof surname);
+const favouriteColour = (prompt("Scrivi il tuo colore preferito"));
+console.log(favouriteColour, typeof favouriteColour);
+
+// Logica del programma
