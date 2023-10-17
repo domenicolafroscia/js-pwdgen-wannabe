2d4061079ff61e985094f0df9f3fefe55b415b9b
domenicolafroscia <domenicolafroscia@gmail.com>
2023-10-17 16:19:26 +0200

    add: termine esercizio

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,16 @@
 // Raccolta di dati
-const name = (prompt("Scrivi il tuo nome"));
-console.log(name, typeof name);
+const username = (prompt("Scrivi il tuo nome"));
+console.log(username, typeof username);
 const surname = (prompt("Scrivi il tuo cognome"));
 console.log(surname, typeof surname);
 const favouriteColour = (prompt("Scrivi il tuo colore preferito"));
 console.log(favouriteColour, typeof favouriteColour);
+const number = 21;
+console.log(number, typeof number);
 
 // Logica del programma
+const message = `${username + surname + favouriteColour + number}`;
+console.log(message);
+
+// Output
+document.getElementById("result").innerHTML = message;
